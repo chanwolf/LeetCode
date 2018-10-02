0ef3887a34d37bcf02a0b092407a03d8378c4cc7
chanwolf <cssy@uci.edu>
2018-10-02 15:37:55 -0700

    removed uncessary steps

--- Valid Palidrome/Solution.java
+++ Valid Palidrome/Solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public static boolean isPalindrome(String s) {
-    	if(s.length() <= 0 )
-    		return true;
-
-
-    	String parsedS = s.replaceAll("[^A-Za-z]", "").toLowerCase();
-    	System.out.println("Stripped version: " + parsedS);
+    	String parsedS = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        if(parsedS.length() <= 1)
+            return true;
+       
 
     	for(int i=0; i<parsedS.length()/2; ++i){
     		int endHalf = parsedS.length()-i-1;
-    		System.out.println(i + " -> " + endHalf);
 
     		if(parsedS.charAt(i) != parsedS.charAt(endHalf)){
     			return false;
@@ -17,11 +14,5 @@ class Solution {
     	}
     	return true;
         
-    }
-
-    static public void main(String[] s){
-    	String pala = "A man, a plan, a canal: Panama";
-    	System.out.println(Solution.isPalindrome(pala));
-
     }
 }
