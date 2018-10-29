0692c092ffb3f05813a1f3b81ca7cdbc0588fc4c
chanwolf <cssy@uci.edu>
2018-10-29 12:27:55 -0700

    changed

--- Max Profit/Solution.java
+++ Max Profit/Solution.java
@@ -4,6 +4,7 @@ class Solution {
  		int buyPoint = Integer.MAX_VALUE;
  		for(int i=0; i<prices.length; ++i){
  			int profit = prices[i] - buyPoint;
+ 			
  			if(prices[i] < buyPoint){
  				buyPoint = prices[i];
  			}
